d083c89f8d81cd6e9a69ee7e9035e6d5dbccaa46
Swagger0214 <3269018758@qq.com>
2026-09-14 11:31:01 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,2 @@
-// 把 Supabase 项目的 URL 和 anon public key 填在这里。
-// 不要填写 service_role key。
-window.SUPABASE_URL = "YOUR_SUPABASE_URL";
-window.SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+window.SUPABASE_URL = "https://vjzyvpkczebfwrqzszoa.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_QtHSmYCzi7aZxxPKLvbNDg_ryptI0ym";
